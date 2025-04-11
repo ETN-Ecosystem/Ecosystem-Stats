2aa2ebf1113885d64ac06eb53b48192a0cef667c
jasonpeters.ton <30388861+jayp89@users.noreply.github.com>
2025-04-12 02:48:51 +0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -13,33 +13,31 @@ export const SBT_COLLECTIONS = [
 
 export const ECOSYSTEM_WALLETS = [
   {
-    purpose: 'ETN-Eco',
+    purpose: 'ETN-Eco Project Wallet',
     address: 'UQA7KhoTClG9QR7DwN1RGAkWIRU0dNp_R8F615y5PiEGQDEK',
-    amount: '204,590 ETN'
   },
   {
     purpose: '🔒Airdrop/Presale',
     address: 'UQAeTw5D90FXWcQpJNzqRpMqK9W4JvLQo4N0ov4f1gV4ZYOQ',
-    amount: '2,000,000 ETN'
   },
   {
     purpose: '🔒Dev Fund',
     address: 'EQAsIR7HgTyaTksfgdJiHWWNyaBrzwj-vuRZhhnVFhYUf_bT',
-    amount: '1,000,000 ETN'
   },
   {
     purpose: 'STON.fi Dex',
     address: 'EQB3ncyBUTjZUA5EnFKR5_EnOMI9V1tTEAAPaiU71gc4TiUt',
-    amount: '351,337 ETN'
   },
+   {
+    purpose: '🔒Stonfi Reserve',
+    address: 'EQBt7drPhWyQRIVnSkDzThhiVcq1lwPT371DYX-gGHzN9bwh',
+   },
     {
     purpose: '🔒JVault Staking',
     address: 'EQCI_ln0rbVRndDaohvqV-m6_0pVec_SHimkM-2qod95n-BK',
-    amount: '414,272 ETN'
   },
   {
     purpose: 'ETN Learn Wallet',
     address: 'UQBImMCDcWNfBmHFm1EenLLO5KHxvE9_IlJZtczzKmC5cC9x',
-    amount: '10,000 ETN'
   }
 ];
