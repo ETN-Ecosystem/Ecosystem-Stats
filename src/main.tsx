5eecadc8909ce500420186fb25bd7d3ddf8b720d
jasonpeters.ton <30388861+jayp89@users.noreply.github.com>
2025-04-18 13:02:13 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import Heartbeat from './components/Heartbeat';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <Heartbeat />
+      <App />
+    </>
   </StrictMode>
 );
